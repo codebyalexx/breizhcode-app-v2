82e378f7d9e4f0be00bc85e763cee48a005df29e
codebyalexx <a.legal.contact@gmail.com>
2024-01-10 06:43:04 +0100

    :bug: Fixed router push in homepage

--- app/(withLayout)/page.tsx
+++ app/(withLayout)/page.tsx
@@ -1,9 +1,14 @@
 'use client';
 
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
     const router = useRouter()
-    router.push('/shop')
+    
+    useEffect(() => {
+        router.push('/shop')  
+    })
+
     return null
 }
