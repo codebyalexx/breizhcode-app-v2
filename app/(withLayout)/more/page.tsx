6e200e7a4bcc790e30bbd6e3c418555b46ee73ce
codebyalexx <a.legal.contact@gmail.com>
2024-01-10 04:34:57 +0100

    :construction: Added cart and more page errors

---
+++ app/(withLayout)/more/page.tsx
@@ -0,0 +1,6 @@
+export default function MorePage() {
+    return (<div className="p-4">
+        <p>Une erreur est survenue lors de l&apos;affichage de votre profil utilisateur.</p>
+        <span className="text-xs text-muted">Error Code 0x8854147512xz7f</span>
+    </div>)
+}
