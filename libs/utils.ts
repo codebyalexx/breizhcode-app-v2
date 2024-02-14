c67687eb389e1dcf840096ece77f57e1f16567c4
AlexandreLG2022 <a.legal.contact@gmail.com>
2024-02-14 05:26:15 +0100

    :triangular_flag_on_post: Added leading zeros to dates & times

--- libs/utils.ts
+++ libs/utils.ts
@@ -1,6 +1,10 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-    return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function padWithLeadingZeros(number: number, length: number = 2) {
+  return String(number).padStart(length, "0");
 }
