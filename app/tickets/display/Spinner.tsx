4e46f06cfbc50c2edb07eb0f5b6947ff444e5a39
AlexandreLG2022 <a.legal.contact@gmail.com>
2024-06-11 03:22:45 +0200

    :zap: Improved spinner animation duration

--- app/tickets/display/Spinner.tsx
+++ app/tickets/display/Spinner.tsx
@@ -1,40 +1,71 @@
-'use client';
-import {useRef} from "react";
+"use client";
+import { useRef } from "react";
 
 export const Spinner = () => {
-    const ref1 = useRef();
-    const ref2 = useRef();
+  const ref1 = useRef();
+  const ref2 = useRef();
 
-    return (<div className={'w-52 h-52 relative'} onTouchStart={(e) => {
+  return (
+    <div
+      className={"w-52 h-52 relative"}
+      onTouchStart={(e) => {
         // @ts-ignore
-        ref1.current.style = `position: fixed; left: ${Math.round(e.touches[0].clientX)-30}px; top: ${Math.round(e.touches[0].clientY)}px;  scale: 1.2;`;
+        ref1.current.style = `position: fixed; left: ${
+          Math.round(e.touches[0].clientX) - 30
+        }px; top: ${Math.round(e.touches[0].clientY)}px;  scale: 1.2;`;
         // @ts-ignore
-        ref2.current.style = `position: fixed; left: ${Math.round(e.touches[0].clientX)-30}px; top: ${Math.round(e.touches[0].clientY)}px;  scale: 1.2;`;
-    }} onTouchEnd={() => {
+        ref2.current.style = `position: fixed; left: ${
+          Math.round(e.touches[0].clientX) - 30
+        }px; top: ${Math.round(e.touches[0].clientY)}px;  scale: 1.2;`;
+      }}
+      onTouchEnd={() => {
         // @ts-ignore
         ref1.current.style = `position: absolute; scale: 1.2;`;
         // @ts-ignore
         ref2.current.style = `position: absolute; scale: 1.2;`;
-    }} onTouchMove={(e) => {
+      }}
+      onTouchMove={(e) => {
         // @ts-ignore
-        ref1.current.style = `position: fixed;left: ${Math.round(e.touches[0].clientX)-30}px; top: ${Math.round(e.touches[0].clientY)}px`;
+        ref1.current.style = `position: fixed;left: ${
+          Math.round(e.touches[0].clientX) - 30
+        }px; top: ${Math.round(e.touches[0].clientY)}px`;
         // @ts-ignore
-        ref2.current.style = `position: fixed; left: ${Math.round(e.touches[0].clientX)-30}px; top: ${Math.round(e.touches[0].clientY)}px`;
-    }} >
-        <img src="/ticketcercle.png" alt="ticket circle sample icon" className={'w-full max-w-52 h-full max-h-52'}/>
-        {/* @ts-ignore */}
-        <span className={'absolute top-20 left-0 h-9 w-[100%] animate-spin flex justify-between max-w-52 max-h-52'} ref={ref1} style={{
-            scale: '1.2'
-        }}>
-                            <span></span>
-                            <span className={'block bg-[#c6e011] w-9 h-9 rounded-full'}></span>
-                        </span>
-        {/* @ts-ignore */}
-        <span className={'absolute top-20 right-0 h-9 w-[100%] animate-spin max-w-52 max-h-52'} ref={ref2} style={{
-            scale: '1.2'
-        }}>
-                            <span className={'block bg-[#b93400] w-9 h-9 rounded-full'}></span>
-                            <span></span>
-                        </span>
-    </div>)
-}
+        ref2.current.style = `position: fixed; left: ${
+          Math.round(e.touches[0].clientX) - 30
+        }px; top: ${Math.round(e.touches[0].clientY)}px`;
+      }}
+    >
+      <img
+        src="/ticketcercle.png"
+        alt="ticket circle sample icon"
+        className={"w-full max-w-52 h-full max-h-52"}
+      />
+      {/* @ts-ignore */}
+      <span
+        className={
+          "absolute top-20 left-0 h-9 w-[100%] animate-spin [animation-duration:1750ms] flex justify-between max-w-52 max-h-52"
+        }
+        ref={ref1}
+        style={{
+          scale: "1.2",
+        }}
+      >
+        <span></span>
+        <span className={"block bg-[#c6e011] w-9 h-9 rounded-full"}></span>
+      </span>
+      {/* @ts-ignore */}
+      <span
+        className={
+          "absolute top-20 right-0 h-9 w-[100%] animate-spin [animation-duration:1750ms] max-w-52 max-h-52"
+        }
+        ref={ref2}
+        style={{
+          scale: "1.2",
+        }}
+      >
+        <span className={"block bg-[#b93400] w-9 h-9 rounded-full"}></span>
+        <span></span>
+      </span>
+    </div>
+  );
+};
